7b7e84b164825e57be3f3ec65cce83ccc97617af
Josh Palmer <91572133+josh-palmerk@users.noreply.github.com>
2025-12-06 11:45:46 -0700

    Stars

--- 231.09.Lab/star.h
+++ 231.09.Lab/star.h
@@ -1,24 +1,47 @@
 /***********************************************************************
  * Header File:
- *    Star
+ *    STAR
  * Author:
- *    Garrin and Josh
+ *    Br. Helfrich
  * Summary:
- *    Background stars.
+ *    A single star that twinkles
  ************************************************************************/
 
 #pragma once
+#include "spaceObject.h" // for SPACEOBJECT
+#include "position.h"   // for POSITION
+#include "uiDraw.h"     // for RANDOM and DRAWSTAR
 
-
-class TestStar;
-
-class Star
+ /*****************************************************
+  * STAR
+  * A star that twinkles
+  *****************************************************/
+class Star : public SpaceObject
 {
 public:
-	friend TestStar;
-	Star();
+	Star() {
+		phase = 0;
+	}
 
-	//TO DO add methods and attributes
-	//void draw(ogstream& ui) const override;
-	//void shatter(vector<SpaceObject*>& spaceObjects) override;
+	Star(double width, double height)
+	{
+		pos.setPixelsX(random(-width, width));
+		pos.setPixelsY(random(-height, height));
+		phase = random(0, 255); // randomize twinkle phase
+	};
+	void draw(ogstream& gout) const override
+	{
+		gout.drawStar(pos, phase);
+	};
+	void incrementPhase() 
+	{
+		phase++; // Increment phase and wrap around at 256
+	}
+	void updateObject(double timestep, vector<SpaceObject*>& spaceObjects) override
+	{
+		incrementPhase();
+	}
+private:
+	Position pos;
+	unsigned char phase;
 };
